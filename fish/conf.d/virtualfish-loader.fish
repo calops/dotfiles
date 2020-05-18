885e759d0217e82900c1606ff563919fd81bc9ed
Rémi Labeyrie <rlabeyrie@payplug.com>
2020-05-18 11:47:46 +0200

    Update virtualfish

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,9 @@
-set -g VIRTUALFISH_VERSION 2.0.1
+set -g VIRTUALFISH_VERSION 2.2.2
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python3
-source /home/calops/.local/lib/python3.7/site-packages/virtualfish/virtual.fish
+source /home/calops/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
+source /home/calops/.local/lib/python3.8/site-packages/virtualfish/compat_aliases.fish
+source /home/calops/.local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
+source /home/calops/.local/lib/python3.8/site-packages/virtualfish/projects.fish
+source /home/calops/.local/lib/python3.8/site-packages/virtualfish/environment.fish
+source /home/calops/.local/lib/python3.8/site-packages/virtualfish/update_python.fish
 emit virtualfish_did_setup_plugins
