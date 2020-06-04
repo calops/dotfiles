98ff29bd4fc18247338d9f1843507cd957e9f124
Rémi Labeyrie <rlabeyrie@payplug.com>
2020-06-04 15:11:04 +0200

    Add fish-ssh-agent

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ alias ls="exa --icons"
 alias ll="ls -lH --git --time-style=long-iso"
 alias la="ll -la"
 alias lt="ls -T"
+alias copy="xclip -selection clipboard"
 
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.pyenv/bin $PATH
@@ -18,3 +19,5 @@ set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -gx FZF_DEFAULT_OPTS "--ansi --preview-window noborder --preview-window 'right:50%' --preview 'bat --color=always --style=grid --line-range :300 {}'"
 
 starship init fish | source
+
+fish_ssh_agent
