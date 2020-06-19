4fad5003a0309e154b6b37ef9b4a9d3310b9f8f5
Rémi Labeyrie <rlabeyrie@payplug.com>
2020-06-19 09:03:37 +0200

    Add script to setup monitors with xrandr

---
+++ scripts/monitors.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+xrandr --output eDP-1 --mode 1920x1080 --pos 2496x1404 \
+       --output DP-1 --scale 1.3x1.3 --mode 1920x1080 --pos 0x0 \
+       --output DP-2 --scale 1.3x1.3 --mode 1920x1080 --pos 2496x0
