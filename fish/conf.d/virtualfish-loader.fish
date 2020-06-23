fe245c5da28bd3f8c67d08dab0fa1a3992eb3515
Rémi Labeyrie <rlabeyrie@payplug.com>
2020-06-23 14:55:28 +0200

    Make fish config more system-agnostic

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,9 +1,9 @@
 set -g VIRTUALFISH_VERSION 2.2.2
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python3
-source /home/calops/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
-source /home/calops/.local/lib/python3.8/site-packages/virtualfish/compat_aliases.fish
-source /home/calops/.local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
-source /home/calops/.local/lib/python3.8/site-packages/virtualfish/projects.fish
-source /home/calops/.local/lib/python3.8/site-packages/virtualfish/environment.fish
-source /home/calops/.local/lib/python3.8/site-packages/virtualfish/update_python.fish
+source ~/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
+source ~/.local/lib/python3.8/site-packages/virtualfish/compat_aliases.fish
+source ~/.local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
+source ~/.local/lib/python3.8/site-packages/virtualfish/projects.fish
+source ~/.local/lib/python3.8/site-packages/virtualfish/environment.fish
+source ~/.local/lib/python3.8/site-packages/virtualfish/update_python.fish
 emit virtualfish_did_setup_plugins
