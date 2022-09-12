65c14447f41b41a263a9a562dd96557163ac220c
Rémi Labeyrie <remi.labeyrie@stockly.ai>
2022-09-12 18:46:28 +0200

    fix: bypass neotree when saving session

--- vim/init.lua
+++ vim/init.lua
@@ -548,9 +548,13 @@ packer.startup(function(use)
     -- Session management
     use {
         'rmagatti/auto-session',
-        config = function() require('auto-session').setup {
-            cwd_change_handling = false,
-        } end,
+        config = function()
+            require('auto-session').setup {
+                cwd_change_handling = false,
+                bypass_session_save_file_types = { "neo-tree" },
+                pre_save_cmds = { function () require 'neo-tree.sources.manager'.close_all() end },
+            }
+        end,
     }
 
     -- Modern notification popups
