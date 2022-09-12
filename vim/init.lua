1a477bdfb1791c1ee8b269b0ed4999b4382307aa
Rémi Labeyrie <remi.labeyrie@stockly.ai>
2022-09-12 18:38:03 +0200

    feat: add null-ls

--- vim/init.lua
+++ vim/init.lua
@@ -62,6 +62,19 @@ packer.startup(function(use)
     }
     use 'williamboman/mason-lspconfig.nvim'
 
+    use {
+        'jose-elias-alvarez/null-ls.nvim',
+        config = function ()
+            local nls = require('null-ls')
+            nls.setup({
+                sources = {
+                    nls.builtins.formatting.stylua,
+                    nls.builtins.diagnostics.buf,
+                },
+            })
+        end,
+    }
+
     -- Language servers
     use 'neovim/nvim-lspconfig'
 
