af3202fb497849b6adf475f07bace1399100bb7c
Rémi Labeyrie <remi.labeyrie@stockly.ai>
2022-10-24 10:35:26 +0200

    update plugins

--- vim/init.lua
+++ vim/init.lua
@@ -786,7 +786,7 @@ mason.setup({
 
 local on_attach = function(client) end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require("lspconfig")
 
 lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
