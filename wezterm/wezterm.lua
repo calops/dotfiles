beebdad73e6e00e98150ad70300ccca4ae2855f6
Rémi Labeyrie <remi.labeyrie@stockly.ai>
2023-04-06 11:29:25 +0200

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,29 +1,46 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 return {
-    font = wezterm.font('Iosevka'),
-    font_size = 10.5,
-    enable_tab_bar = false,
-    color_scheme = "Catppuccin Mocha",
-    window_padding = {
-        left = 0,
-        bottom = 3,
-        top = 3,
-        right = 0,
-    },
-    -- window_background_opacity = 0.7,
-    -- text_background_opacity = 1,
-    keys = {
-        {
-            key = 'Tab',
-            mods = 'CTRL',
-            action = act.SendKey { key = 'Tab', mods = 'CTRL' },
-        },
-        {
-            key = 'Tab',
-            mods = 'CTRL|SHIFT',
-            action = act.SendKey { key = 'Tab', mods = 'CTRL|SHIFT' },
-        },
-  },
+	font = wezterm.font("Iosevka"),
+	font_size = 10,
+	underline_thickness = 2,
+	underline_position = -2,
+	enable_tab_bar = false,
+	color_scheme = "Catppuccin Mocha",
+	allow_square_glyphs_to_overflow_width = "Always",
+	animation_fps = 30,
+	cursor_blink_rate = 500,
+	default_cursor_style = "BlinkingBlock",
+	window_padding = {
+		left = 0,
+		bottom = 0,
+		top = 0,
+		right = 0,
+	},
+	visual_bell = {
+		fade_in_function = "Ease",
+		fade_in_duration_ms = 150,
+		fade_out_function = "Ease",
+		fade_out_duration_ms = 150,
+	},
+	keys = {
+		{
+			key = "Tab",
+			mods = "CTRL",
+			action = act.SendKey({ key = "Tab", mods = "CTRL" }),
+		},
+		{
+			key = "Tab",
+			mods = "CTRL|SHIFT",
+			action = act.SendKey({ key = "Tab", mods = "CTRL|SHIFT" }),
+		},
+	},
+	ssh_domains = {
+		{
+			name = "charybdis",
+			remote_address = "charybdis",
+            remote_wezterm_path = "~/.local/bin/wezterm"
+		},
+	},
 }
